72806dd379da0b0b9048ed104e193e3c2109db7a
Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2024-05-01 14:03:39 -0500

    Enable route to send request in main with CORS

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = (origin: string) => {
+    // Check if the origin is in a list of allowed origins
+    const whitelist = ['http://127.0.0.1:8080', 'http://localhost:3000']; // Add your allowed origins here
+    return whitelist.includes(origin);
+  };
+
     app.enableCors({
-      origin: 'http://localhost:3000', // Allow requests from the frontend server
+
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
       credentials: true, // Allow credentials (cookies, authorization headers, etc.) to be sent with the request
     });
 
